cabfb7e4670e5b71c51c6695f5a3bdbb42d606f8
Ryan <ruihang001@e.ntu.edu.sg>
2020-04-10 22:48:05 +0800

    fix savefig

--- plot.py
+++ plot.py
@@ -7,23 +7,21 @@ from config import IN_FILENAME, OUT_FILENAME, RESULT_PATH
 
 
 def plot():
-
     data_points, data_points_x, data_points_y = read_in_txt(IN_FILENAME)
-    degree, cnt_num, knots_list, \
-    control_points_x, control_points_y = read_out_txt(OUT_FILENAME)
+    degree, cnt_num, knots_list, control_points_x, control_points_y = read_out_txt(OUT_FILENAME)
 
     xx = np.linspace(0.01, 0.99, 100)
 
     fig, ax = plt.subplots()
-    x = [bspline(x, knots_list, control_points_x , degree) for x in xx]
-    y = [bspline(x, knots_list, control_points_y , degree) for x in xx]
+    x = [bspline(x, knots_list, control_points_x, degree) for x in xx]
+    y = [bspline(x, knots_list, control_points_y, degree) for x in xx]
 
-    ax.plot(control_points_x, control_points_y, 'k--', marker='s',markerfacecolor='r', label='control points')
+    ax.plot(control_points_x, control_points_y, 'k--', marker='s', markerfacecolor='r', label='control points')
     ax.plot(data_points_x, data_points_y, ' ', marker='o', markerfacecolor='c', label='data points')
     ax.plot(x, y, 'g-', lw=2, label='B-Spline')
     ax.grid(True)
     ax.legend(loc='best')
-    plt.savefig(os.path.join(RESULT_PATH, OUT_FILENAME[0:-4]+'png'))
+    plt.savefig(os.path.join(RESULT_PATH, OUT_FILENAME[0:-4] + '.png'))
     plt.show()
 
 
